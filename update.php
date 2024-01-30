abeff87e907715b7e8992512813a9e348d09be2b
Bhim Prasad Lamichhane <dreamypd73@gmail.com>
2024-01-30 17:42:24 +0545

    minor changes in updates

--- update.php
+++ update.php
@@ -69,17 +69,17 @@ try {
     $address = $data->address;
     $salary = $data->salary;
 
-    $sql = "UPDATE student SET name='$name', email='$email', address='$address', age='$age' WHERE id=$id";
+    $sql = "UPDATE student SET name='$name', email='$email', address='$address', salary='$salary' WHERE id=$id";
     $result = $db->conn->query($sql);
 
     if ($result) {
         http_response_code(200);
         $updated_data = [
-            "id" => $id,
+            
             "name" => $name,
             "email" => $email,
             "address" => $address,
-            "age" => $age
+            "salary" => $salary
         ];
         echo json_encode(
             [
